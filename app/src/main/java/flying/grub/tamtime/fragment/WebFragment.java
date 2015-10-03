25b732d9877fdb432368232ca26c7cd7dd58496c
flyingrub <flyinggrub@gmail.com>
2015-10-03 23:23:41 +0200

    TEST

--- app/src/main/java/flying/grub/tamtime/fragment/WebFragment.java
+++ app/src/main/java/flying/grub/tamtime/fragment/WebFragment.java
@@ -1,18 +1,12 @@
 package flying.grub.tamtime.fragment;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.CookieManager;
-import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
